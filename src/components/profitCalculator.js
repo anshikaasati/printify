a357a5e494b52a511db5ab7f436e086334805ef2
Anshika Asati <134847046+anshikaasati@users.noreply.github.com>
2024-09-23 13:36:00 +0530

    first commit

--- src/components/profitCalculator.js
+++ src/components/profitCalculator.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import profit from '../assets/image_converted.jpg';
 import './ts.css'; // Ensure you have this import for your CSS
 
 const ProfitCalculator = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setIsVisible(true); // Set the component to visible when it comes into view
+          observer.unobserve(entry.target); // Stop observing once it's visible
+        }
+      });
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current); // Start observing the component
+    }
+
+    return () => {
+      if (ref.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(ref.current); // Clean up the observer on unmount
+      }
+    };
+  }, []);
+
   return (
     <div className="flex items-center justify-center py-12 max-w-[1060px] mx-auto">
-      <div className="container flex flex-col md:flex-row items-center bg-gray-900 text-white rounded-lg shadow-lg overflow-hidden">
+      <div className="container flex flex-col md:flex-row items-center bg-gray-900 text-white rounded-lg shadow-lg overflow-hidden" ref={ref}>
         
         {/* Left Section */}
-        <div className="p-8 md:w-1/2 animate-slide-in-right">
+        <div className={`p-8 z-1 md:w-1/2 ${isVisible ? 'animate-slide-in-right' : 'opacity-0'}`}>
           <h2 className="text-4xl font-bold mb-4">Make Money, Risk-Free</h2>
           <p className="mb-6">You pay for fulfillment only when you make a sale</p>
           
